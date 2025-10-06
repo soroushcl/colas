e39c29066c9e01984d0e11bfe4b026e360d1d31d
soroush-augie <soroush@augie.vet>
2025-10-06 16:19:09 +0330

    Add private_key_id to Google Mailer and Cloud Storage credentials

--- c-api/src/services/mailer/googleMailer/credentials.ts
+++ c-api/src/services/mailer/googleMailer/credentials.ts
@@ -1,6 +1,7 @@
 export const credentials = {
   "type": "service_account",
   "project_id": "steam-bee-376520",
+  "private_key_id": "78ba4f7d2d5b2487762dd57eaf3e82d1b28949a4",
   "client_email": "mailer@steam-bee-376520.iam.gserviceaccount.com",
   "client_id": "115005901297354973407",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
